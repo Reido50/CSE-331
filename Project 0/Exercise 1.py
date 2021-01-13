abea7abdd91ebb57dc26b9c41a00ef8ae14b7389
Reid Harry <harryrei@msu.edu>
2021-01-13 11:33:57 -0600

    Test with VS Code

---
+++ Project 0/Exercise 1.py
@@ -0,0 +1,17 @@
+from typing import List, Any
+
+# do not modify function declaration
+
+"""
+You'll notice that in the function declaration, parameter `data` is given as
+`data: List[Any]`. This is an example of type hinting. The type of `data` is 
+a list containing elements of type `Any`. The return type of the function is given
+by `-> Any`, indicating return type `Any`
+"""
+
+
+def exercise1(data: List[Any]) -> Any:
+    """
+    param data: a list of like objects
+    return: maximal element of `data`
+    """
