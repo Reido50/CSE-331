5097e0ba0e6cd895aa62154e63bb2e0d71210df3
Reid Harry <harryrei@msu.edu>
2021-02-15 12:47:33 -0500

    Added CC4

---
+++ CC4/solution.py
@@ -0,0 +1,14 @@
+"""
+Name
+Coding Challenge 4
+CSE 331 Spring 2021
+Professor Sebnem Onsay
+"""
+from typing import List
+
+
+def challenger_finder(stocks_list: List[int], k: int) -> List[int]:
+    """
+    Fill out doc string
+    """
+    pass
